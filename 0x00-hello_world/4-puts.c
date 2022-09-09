78884318b03b7e401f923e7409b2c4f3735587fa
aliabdela <ardii.2000@gmail.com>
2022-09-09 10:45:44 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/**
+/*
  * main-print the string in the put function
  */
 int main(void)
 {
-puts("\" Programming is like building a multilingual puzzle";
+puts("\" Programming is like building a multilingual puzzle");
 return(0);
 }
