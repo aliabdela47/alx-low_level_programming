602a39dd46fe461eedde740c23b657bc339d24f0
aliabdela <ardii.2000@gmail.com>
2022-09-13 15:19:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *main - print putchar
- *  Return: always 0
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
-	Return (0);
+
+	return (0);
 
 }
