cf9482e85ab673d1a0aedb6aa88903f07fd775a0
aliabdela <ardii.2000@gmail.com>
2022-10-30 10:07:08 -0700

    all

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,7 +4,6 @@ int main(void)
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
 /*
@@ -15,7 +14,6 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-a[2] = 98\n
 ;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
